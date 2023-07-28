048b5f710ad05c25db7680d6a20b0da69c6ab5e1
Burcali Ahmadov <burcaliahmadov97@gmail.com>
2023-07-28 18:40:37 +0400

    NavHost commit

---
+++ app/src/main/java/com/ahmadov/cryptocrazy/view/CryptoListScreen.kt
@@ -0,0 +1,9 @@
+package com.ahmadov.cryptocrazy.view
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CryptoListScreen(navController: NavController){
+    Text(text = "Hello World")
+}
